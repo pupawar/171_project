c249645da75429ab3500f1977a17e5042cc77461
wbaghdadi20 <wbaghdadi5@gmail.com>
2023-12-04 14:43:43 -0800

    Fixed threading issue

--- Website/app.py
+++ Website/app.py
@@ -1,12 +1,10 @@
 from flask import Flask, render_template, request
-import matplotlib.pyplot as plt
 from werkzeug.utils import secure_filename
 from tensorflow.keras.models import load_model
 import numpy as np
 import os
 import io
-from PIL import Image
-import random
+from PIL import Image, ImageDraw
 import threading
 import webbrowser
 
@@ -20,9 +18,6 @@ model = load_model('Website/NewResNet50.h5')
 @app.route('/')
 def index():
     return render_template('index.html')
-
-def sort_pattern(s):
-    return [int(text) if text.isdigit() else text.lower() for text in re.split(r'(\d+)', s)]
 
 def sort_pattern(filename):
     # Extract numerical part of the filename for sorting, assuming the format 'name_number.jpg'
@@ -44,64 +39,56 @@ def load_images():
     return images
 
 def show(image, keypoints):
-    _, axes = plt.subplots(1, dpi=100)
-    axes.imshow(image, cmap='gray')
+
+    if len(image.shape) == 2 or image.shape[2] == 1:  # if grayscale
+        pil_image = Image.fromarray(np.uint8(image)).convert('RGB')
+        print('gray')
+    else:
+        pil_image = Image.fromarray(np.uint8(image))
+        print('color')
+
+    draw = ImageDraw.Draw(pil_image)
 
     keypoints = keypoints.reshape(-1, 2)
-
+    radius = 1
     for point in keypoints:
         x, y = point
-        plt.plot(x, y, 'ro', markersize=5)
+        # Define the bounding box for the circle
+        # The bounding box is a square with sides of length radius*2
+        upper_left = (x - radius, y - radius)
+        bottom_right = (x + radius, y + radius)
+        draw.ellipse([upper_left, bottom_right], fill='red', outline='red')
 
-    axes.axis('off')
-
-    # Save to a temporary buffer
-    buffer = io.BytesIO()
-    plt.savefig(buffer, format='png', bbox_inches='tight', pad_inches=0)
-    plt.close()
-
-    # Resize the image using PIL
-    buffer.seek(0)
-    pil_image = Image.open(buffer)
-    resized_image = pil_image.resize((96, 96), Image.ANTIALIAS)
 
     # Save the resized image
-    result_path = UPLOAD_FOLDER + '/prediction.png'
-    resized_image.save(result_path)
+    result_path = os.path.join(UPLOAD_FOLDER, 'prediction.png')
+    pil_image.save(result_path)
 
 @app.route('/predict', methods=['POST'])
 def predict():
-    # Check if the 'image' file is part of the request
     if 'image' not in request.files:
         return 'No image file provided.'
 
     image = request.files['image']
     
-    # Save the uploaded image
     if image:
-        # save image in uploads
         image_path = os.path.join(app.config['UPLOAD_FOLDER'], '0.jpg')
         image.save(image_path)
 
-        # open image and preprocess
         img = Image.open(image_path).convert('L')
         img = img.resize((96, 96))
-        img_array = np.array(img)/255.0
+        img_array = np.array(img) / 255.0
         img_array = np.expand_dims(img_array, axis=-1)
         img_array = np.expand_dims(img_array, axis=0)
         
-        # idk wtf this is for
         test_img = load_images()
 
-        # more preprocessing :/
         test_img_array = np.stack(test_img, axis=0)
         x_test = test_img_array.reshape(-1, 96, 96, 1).astype('float64')
 
-        # prediction
         y_test = model.predict(x_test).astype('float64')
         image = np.squeeze(x_test, axis=(0, 3))
 
-        # display and save 'prediction.png'
         show(image, y_test)
 
         return 'prediction.png'
